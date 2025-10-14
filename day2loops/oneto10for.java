3262b90c9b6a3df024f0305a6518453a779c9dfa
Kailash <magantikailash@gmail.com>
2025-10-14 21:13:01 +0530

    1 to 10

---
+++ day2loops/oneto10for.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class oneto10for
+{
+    public static void main (String args[])
+    {
+        Scanner Sc=new Scanner (System.in);
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i);
+            Sc.close();
+        }
+    }
+}
